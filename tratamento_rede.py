c81aa501e41f6aa25493b3bbf8eacf0d9fcfdb8f
Giovanni Grizante <giovanni.grizante@gmail.com>
2026-03-11 22:58:51 -0300

    Adicionado tratamento dos dados para aplicação na rede neural, de forma que as transições entre categorias de geração sejam analisadas de forma contínua pela rede.

--- tratamento_rede.py
+++ tratamento_rede.py
@@ -1,4 +1,4 @@
-import os, pandas as pd
+import os, pandas as pd, multiprocessing, sys, numpy as np
 # import tensorflow as tf
 # import h5py
 from sklearn.compose import ColumnTransformer
@@ -7,9 +7,83 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 # from keras import backend as K  #Função para resetar a rede criada
 
 
+def tratamento_dados(iema_recente):
+    # Mesclagem e normalização dos dados por usina
+    for usina in os.listdir('Dados Tratados'):
+        geracao = pd.read_parquet(os.path.join('Dados Tratados', usina, 'Dados Externos', f'ONS.parquet'))
+        emissao = pd.read_parquet(os.path.join('Dados Tratados', usina, 'Emissões Sintéticas', f'Horárias.parquet'))
+        
+        try:
+            unitarios = iema_recente[iema_recente['Usina'] == usina].drop(columns=['Usina']).reset_index(drop=True)
+        except KeyError:
+            unitarios = iema_recente[iema_recente['CEG'] == usina].drop(columns=['CEG']).reset_index(drop=True)
+            
+        # Unificação dos dados de geração, emissão e dados unitários
+        tabela = pd.merge(geracao, emissao, on=None, how='left').merge(unitarios, on=None, how='cross')
+        
+        # Remover colunas de delta. Caso desejar incluir, comentar linha abaixo.
+        tabela = tabela.drop(columns=['Delta menos', 'Delta mais'])
+        
+        
+        # == FEATURES DE TRANSIÇÃO ==
+        # Features importantes para a rede neural tratar as transições como um processo, e não como um estado fixo.
+        
+        # Duração da transição
+        # Flag de transição (1 se estiver em transição, 0 caso contrário)
+        transicao_list = list((tabela['Categoria de geração'] == 0).astype(int))
+        duracao = np.zeros_like(transicao_list, dtype=int)
+        contador = 0
+        for i in range(len(transicao_list)):
+            if transicao_list[i]:
+                contador += 1
+            else:
+                contador = 0
+            duracao[i] = contador
+        tabela['Duração da Transição'] = duracao
+        
+        # Fase da transição
+        fase = np.zeros_like(transicao_list, dtype=int)
+        for i in range(len(transicao_list)):
+            if transicao_list[i]:  # em transição
+                inicio = (i == 0) or (not transicao_list[i-1])  # Verificar se é o início da transição ou se é a primeira linha
+                fim = (i == len(transicao_list) - 1) or (not transicao_list[i+1])  # Verificar se é o fim da transição ou se é a última linha
+                
+                if inicio and fim:
+                    fase[i] = 1  # Transição de apenas uma hora (início e fim ao mesmo tempo)
+                elif inicio:  # hora anterior não era transição
+                    fase[i] = 1  # INÍCIO
+                elif fim:  # próxima hora não será transição
+                    fase[i] = 3  # FIM
+                else:
+                    # MEIO: pode ser hora 2 de 3, ou hora 2 de 4, etc.
+                    fase[i] = 2  # MEIO (qualquer hora que não é início nem fim)
+        tabela['Fase da Transição'] = fase
+        
+        # Definir as colunas categóricas e numéricas para o ColumnTransformer
+        categoricas = ['Categoria de geração']
+        numericas = ['Geração', 'Duração da Transição', 'Fase da Transição']  # Adicionar outras colunas numéricas, se necessário
+        
+        # Transformação da coluna categórica "Categoria de geração" em numérica e normalização da coluna numérica "Geração"
+        transf = ColumnTransformer(transformers=[
+            ('onehot', OneHotEncoder(sparse_output=False), categoricas),
+            ('scaler', StandardScaler(), numericas)],
+                                   remainder='passthrough',  # Mantém constant_cols inalteradas
+                                   verbose_feature_names_out=False  # Para nomes mais limpos
+                                   )
+        
+        data = transf.fit_transform(tabela)
+        
+        # Geração da tabela final
+        tabela = pd.DataFrame(data, columns=transf.get_feature_names_out())
+        
+        # Salvar a tabela unificada em formato parquet
+        os.makedirs(os.path.join('Dados Tratados', usina, 'Rede Neural'), exist_ok=True)
+        tabela.to_parquet(os.path.join('Dados Tratados', usina, 'Rede Neural', 'Dados_Entrada.parquet'))
+        sys.exit()
+        
+
 # Ler a tabela mais recente do IEMA e filtrar os dados para as usinas presentes na pasta "Dados Tratados"
-dir = 'IEMA/Tabelas'
-iema_recente = pd.read_excel(os.path.join(dir, sorted(os.listdir(dir))[-1]))
+iema_recente = pd.read_excel(os.path.join('IEMA/Tabelas', sorted(os.listdir('IEMA/Tabelas'))[-1]))
 
 colunas_remover = ['Município', 
                    'Geração [GWh]', 
@@ -29,27 +103,19 @@ iema_recente = iema_recente.drop(columns=colunas_remover)
 for coluna in ['Potência Instalada', 'Fator de Capacidade [%]', 'Eficiência Energética [%]']:
     iema_recente[coluna] = pd.to_numeric(iema_recente[coluna], errors='coerce')
 
-print(iema_recente.head())
+# Aplicar OneHotEncoder (Transformação das colunas categóricas em numéricas) e manter as colunas numéricas
+# É necessário aplicar esse processo para garantir que sejam analisadas todas as combinações possíveis.
+# Após aplicar os dados em cada usina específica, as combinações se perdem.
+onehot = ColumnTransformer(transformers=[('encoder',OneHotEncoder(),['Combustível','Ciclo de Operação'])],remainder='passthrough')
+data = onehot.fit_transform(iema_recente)
 
-# # Aplicar OneHotEncoder (Transformação das colunas categóricas em numéricas) e manter as colunas numéricas
-# onehot = ColumnTransformer(transformers=[('encoder',OneHotEncoder(),['Combustível','Ciclo de Operação'])],remainder='passthrough')
-# data = onehot.fit_transform(iema_recente)
+# Obter os nomes das colunas resultantes do OneHotEncoder e combinar com as colunas restantes
+nomes_onehot = onehot.named_transformers_['encoder'].get_feature_names_out(['Combustível', 'Ciclo de Operação'])
+colunas_restantes = iema_recente.drop(columns=['Combustível', 'Ciclo de Operação']).columns
+colunas = list(nomes_onehot) + list(colunas_restantes)
 
-# # Aplicar StandardScaler (Padronização das colunas numéricas) e manter as colunas resultantes do OneHotEncoder
-# scaler = ColumnTransformer(transformers=[('scaler',StandardScaler(),['Potência Instalada', 'Fator de Capacidade [%]', 'Eficiência Energética [%]'])],remainder='passthrough')
-# data = scaler.fit_transform(data)
-
-# # Obter os nomes das colunas resultantes do OneHotEncoder e combinar com as colunas restantes
-# nomes_onehot = onehot.named_transformers_['encoder'].get_feature_names_out(['Combustível', 'Ciclo de Operação'])
-# colunas_restantes = iema_recente.drop(columns=['Combustível', 'Ciclo de Operação']).columns
-# colunas = list(nomes_onehot) + list(colunas_restantes)
-
-# iema_recente = pd.DataFrame(data,columns=colunas)
-
-
-
-
-
+iema_recente = pd.DataFrame(data,columns=colunas)
+tratamento_dados(iema_recente)
 
 
 
