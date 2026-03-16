dd6aac03dd5773321f5a5aa1873108ef9c54d403
Giovanni Aurelio <G15548@evonik.com>
2026-03-16 11:55:43 -0300

    Arquivo main para execução dos códigos na ordem correta.

--- main.py
+++ main.py
@@ -5,18 +5,34 @@ import graficos, graficos_aux
 # Ordem de execução dos arquivos.
 
 # Etapa 1 - Tratamento e armazenamento dos dados IEMA e ONS para minimização.
-iema.main()
-ons.main()
-armazenar.main()
+etapa1 = input('Deseja executar a etapa 1? (1 - Sim | 2 - Não) ')
+if etapa1 == '1':
+    iema.main()
+    ons.main()
+    armazenar.main()
+else:
+    pass
 
 #Etapa 2 - Execução da minimização e geração das emissões sintéticas.
-ampl.main()
-sinteticas.main()
+etapa2 = input('Deseja executar a etapa 2? (1 - Sim | 2 - Não) ')
+if etapa2 == '1':
+    ampl.main()
+    sinteticas.main()
+else:
+    pass
 
 # Etapa 3 - Tratamento dos dados, criação e execução da rede neural.
-tratamento_rede.main()
-rede_neural.main()
+etapa3 = input('Deseja executar a etapa 3? (1 - Sim | 2 - Não) ')
+if etapa3 == '1':
+    tratamento_rede.main()
+    rede_neural.main()
+else:
+    pass
 
 # Etapa 4 - Geração dos gráficos. 
-graficos.main()
-graficos_aux.main()
+etapa4 = input('Deseja executar a etapa 4? (1 - Sim | 2 - Não) ')
+if etapa4 == '1':
+    graficos.main()
+    graficos_aux.main()
+else:
+    pass
